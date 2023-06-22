619ee61eb652c44973e963d9f6e36ecf442a58b1
leirevalencia <132055494+leirevalencia@users.noreply.github.com>
2023-06-22 20:40:29 +0200

    week-forecast

--- src/app.js
+++ src/app.js
@@ -39,6 +39,15 @@ let currentDate = document.getElementById("date-time");
 currentDate.innerHTML = `${day} ${date} ${month} / ${hour}:${minute}`;
 
 //Change city, temperature and weather description
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "2d96d64425dca1d6eda00d942a281c0d";
+  let apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+  console.log(apiURL);
+  axios.get(apiURL).then(displayForecast);
+}
+
 function showParameters(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector ("#temperature");
@@ -48,6 +57,9 @@ function showParameters(response) {
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
   iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+  getForecast(response.data.coord); //nueva función para obtener las coordenadas
+
 }
 
 function search(city) {
@@ -93,3 +105,46 @@ fahrenheit.addEventListener("click", displayFahrenheit);
 
 let celsisus = document.querySelector("#celsius");
 celsisus.addEventListener("click", displayCelsius);
+
+//display forecast (days of the week)
+function displayForecast(response) {
+  console.log(response.data);
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class ="col">`;  //, crea una columna con una línea con varias columnas;hacemos un loop para mostrar lo que queramos varias veces.
+  //en la siguiente línea si añado la cadena anterior parecerá lo de la cadena anterior y el bloque siguiente
+  
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"]; //crea un loop para cada día, y pone cada día dentro de la variable day
+  days.forEach(function (day) {
+    forecastHTML = forecastHTML + `  
+  <div class="weather-forecast" id="forecast">
+  <div class="monday">
+      <div class="container">
+          <div class="row">
+              <div class="col-4" id="weather-forecast-date">
+                  ${day}
+              </div>
+              <div class="col-4"> 
+                  <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="" id="icon"/>
+              </div>
+              <div class="col-4" id="weather-forecast-temperature">
+                  18
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+`;
+  });
+  
+ //con esto hacemos que forecastElement muestre en nuestra página (en HTML) una copia del forecast
+
+
+
+forecastHTML = forecastHTML + `</div>`; //lo repetimos dos veces, y cerramos div
+
+  forecastElement.innerHTML = forecastHTML; 
+} //para que muestre lo mismo todas las veces que queramos, hacemos un loop
+displayForecast();
+
+//src="" alt="Clear" id="icon" class="float-left"
